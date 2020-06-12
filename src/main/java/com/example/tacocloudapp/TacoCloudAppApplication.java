082e9776838ff837dc82b79d88be07624344ccd9
harishankarsivaji <harishs92@gmail.com>
2020-06-12 16:46:35 +0100

    Initial commit

---
+++ src/main/java/com/example/tacocloudapp/TacoCloudAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.tacocloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TacoCloudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudAppApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/tacocloudapp/TacoCloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
